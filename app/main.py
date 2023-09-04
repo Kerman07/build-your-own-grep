9d788f6fda49882aa95f2a0ef656ddc297fbb776
Kerim Mandzo <kerim.mandzo@klika.ba>
2023-09-04 18:43:28 +0200

    pass positive_character_groups tests

--- app/main.py
+++ app/main.py
@@ -4,13 +4,28 @@ import sys
 # import lark - available if you need it!
 
 
+def match_digit(input_line):
+    return any(char.isdigit() for char in input_line)
+
+
+def match_alphanumeric(input_line):
+    return any(char.isalnum() for char in input_line)
+
+
+def match_positive_character_groups(input_line, pattern):
+    chars_in_groups = "".join([group.split("]")[0] for group in pattern.split("[")])
+    return any(char in input_line for char in chars_in_groups)
+
+
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
     elif pattern == "\d":
-        return any(char.isdigit() for char in input_line)
+        return match_digit(input_line)
     elif pattern == "\w":
-        return any(char.isalnum() for char in input_line)
+        return match_alphanumeric(input_line)
+    elif "[" in pattern and "]" in pattern:
+        return match_positive_character_groups(input_line, pattern)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
